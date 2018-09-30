2e1253bf3008db78af252f364bfa90a1fc09e945
dongxuxuan <3215057@qq.com>
2018-09-30 17:01:01 +0800

    2018/9/30 happy~the holiday of China will come

--- LintCode/GoSrc/src/LogicFun/fun1.go
+++ LintCode/GoSrc/src/LogicFun/fun1.go
@@ -1,6 +1,11 @@
 package LogicFun
 
-import "CommonFun"
+import (
+	"CommonFun"
+	"math"
+	"strconv"
+	"strings"
+)
 
 // 1282. Reverse Vowels of a String
 func ReverseVowels(s string) string {
@@ -52,4 +57,59 @@ func ReverseVowels(s string) string {
 	}
 
 	return string(resBytes)
+}
+
+// 1385. Lucky Number Eight
+func LuckyNumber(n int) int {
+	res := 0
+	for i := 1; i <= n; i++ {
+		if strings.Contains(strconv.Itoa(i), "8") {
+			res++
+		}
+	}
+
+	return res
+}
+
+// 914. Flip Game
+func GeneratePossibleNextMoves(s string) []string {
+	res := make([]string, 0, 0)
+	strByte := []byte(s)
+	for i := len(s) - 1; i > 0; i-- {
+		if s[i] == '+' && s[i-1] == '+' {
+			strByte[i-1] = '-'
+			strByte[i] = '-'
+			res = append(res, string(strByte))
+			strByte[i-1] = '+'
+			strByte[i] = '+'
+		}
+	}
+
+	return res
+}
+
+// 1314. Power of Two
+func IsPowerOfTwo(n int) bool {
+	for n >= 1 {
+		if n == 2 || n == 1 {
+			return true
+		}
+		if n%2 == 0 {
+			n /= 2
+		} else {
+			return false
+		}
+	}
+	return false
+}
+
+// 1228. Poor Pigs
+func PoorPigs(buckets int, minutesToDie int, minutesToTest int) int {
+	time := minutesToTest/minutesToDie + 1
+	res := 0
+	for math.Pow(float64(time), float64(res)) < float64(buckets) {
+		res++
+	}
+
+	return res
 }
--- LintCode/GoSrc/src/main.go
+++ LintCode/GoSrc/src/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(LogicFun.ReverseVowels("lintcode"))
+	fmt.Println(LogicFun.GeneratePossibleNextMoves(""))
 }
