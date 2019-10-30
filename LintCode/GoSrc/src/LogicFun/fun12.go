4dca9191f2a763a6404882331287d17944e0c535
[dongxuxuan] <[donxuxuan@kingsoft.com]>
2019-10-30 15:09:21 +0800

    [bugfix] spiral matrix

--- LintCode/GoSrc/src/LogicFun/fun12.go
+++ LintCode/GoSrc/src/LogicFun/fun12.go
@@ -4,20 +4,32 @@ package LogicFun
 // 54. Spiral Matrix
 func SpiralOrder(matrix [][]int) []int {
 	row := len(matrix)
+	if row == 0 {
+		return nil
+	}
+
 	col := len(matrix[0])
 	res := make([]int, row*col)
 	n := 0
 	for start := 0; start < (row+1)/2 && start < (col+1)/2; start++ {
 		i := start
 		j := start
-		if i == col-start-1 && j == col-start-1 {
+		cj := false
+		if i == row-start-1 && j == col-start-1 {
 			res[n] = matrix[i][j]
 			continue
 		}
 
+		if j == col-start-1 {
+			cj = true
+		}
 		for ; j < col-start-1; j++ {
 			res[n] = matrix[i][j]
 			n++
+		}
+		if i == row-start-1 {
+			res[n] = matrix[i][j]
+			continue
 		}
 		for ; i < row-start-1; i++ {
 			res[n] = matrix[i][j]
@@ -26,6 +38,10 @@ func SpiralOrder(matrix [][]int) []int {
 		for ; j > start; j-- {
 			res[n] = matrix[i][j]
 			n++
+		}
+		if cj {
+			res[n] = matrix[i][j]
+			continue
 		}
 		for ; i > start; i-- {
 			res[n] = matrix[i][j]
--- LintCode/GoSrc/src/main.go
+++ LintCode/GoSrc/src/main.go
@@ -10,5 +10,5 @@ func main() {
 	// aaa := make([]int, 0, 10)
 	// aaa = append(aaa, 10)
 	// println(aaa)
-	LogicFun.SpiralOrder([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
+	LogicFun.SpiralOrder([][]int{{1}, {2}, {3}})
 }
